1aa2e853e2ebafce71b8b58be729bbfe37a51d2f
Joscelyn James <josce.james7@hotmail.com>
2020-07-21 22:29:48 -0600

    feat: wired up covid service to chart component

--- src/app/shared/chart/chart.component.ts
+++ src/app/shared/chart/chart.component.ts
@@ -1,5 +1,6 @@
 import { Component, NgZone, Input } from "@angular/core";
-import { AmChartService } from 'src/app/services/am-chart.service';
+import { AmChartService, Sprite, ChartData } from 'src/app/services/am-chart.service';
+import { CovidService } from 'src/app/services/api/covid.service';
 
 @Component({
   selector: "chart",
@@ -7,24 +8,28 @@ import { AmChartService } from 'src/app/services/am-chart.service';
   styleUrls: ["./chart.component.scss"]
 })
 export class ChartComponent {
-  private chart: any;
+  private chart: Sprite;
 
-  @Input() data: Array<{date: Date; name: string; value: number}> // come back to type this
+  @Input() data: Array<ChartData>
 
-  constructor(private zone: NgZone, private chartService: AmChartService) {
+  constructor(
+    private zone: NgZone, 
+    private chartService: AmChartService, 
+    private covidService: CovidService
+  ) {
     this.chartService.useDarkTheme()
   }
-
-  ngAfterViewInit() {
-    this.zone.runOutsideAngular(() => {
+  
+  async ngAfterViewInit() {
+    this.zone.runOutsideAngular(async () => {
       this.chart = this.chartService.create("chart-div", this.chartService.XYChart);
+
+      this.chart.data = await this.covidService.getStatsForUnitedStates();
 
       this.chart.paddingRight = 30;
       this.chart.paddingBottom = 30;
       this.chart.paddingLeft = 10;
-
-      this.chart.data = this.data;
-
+      
       const dateAxis = this.chart.xAxes.push(this.chartService.DateAxis());
       dateAxis.renderer.grid.template.location = 0;
 
@@ -32,7 +37,7 @@ export class ChartComponent {
 
       const series = this.chart.series.push(this.chartService.LineSeries());
       series.dataFields.dateX = "date";
-      series.dataFields.valueY = "value";
+      series.dataFields.valueY = "casesConfirmedOnThisDay";
       series.tooltipText = "{valueY.value}";
 
       this.chart.cursor = this.chartService.XYCursor();
