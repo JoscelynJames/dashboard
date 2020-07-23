12231efc393482f17806759162f4daf1aa5f71c7
Joscelyn James <josce.james7@hotmail.com>
2020-07-23 08:45:43 -0600

    feature: added fake data for the graph and only will call api when it is production

--- src/app/services/api/covid.service.ts
+++ src/app/services/api/covid.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core'
 import axios from 'axios'
+import { environment } from '../../../environments/environment';
 
 @Injectable()
 export class CovidService {
@@ -10,8 +11,9 @@ export class CovidService {
 
   async getStatsForUnitedStates() {
     try {
+      if (!environment.production) return this.fakeData()
+
       const { data } = await axios.get('https://api.covid19api.com/total/country/united-states')
-  
       this.unitedStatesCovidData = data
 
       return this.formatChartData()
@@ -34,7 +36,18 @@ export class CovidService {
       }
     })
   }
+
+  fakeData() {
+    let data = [];
+    let visits = 10;
+    for (let i = 1; i < 366; i++) {
+      visits += Math.round((Math.random() < 0.5 ? 1 : -1) * Math.random() * 10);
+      data.push({ date: new Date(2018, 0, i), name: "name" + i, casesConfirmedOnThisDay: visits, value: visits });
+    }
+    return data 
+  }
 }
+
 
 interface UnitedStatesCovidDataPoint {
   Date: Date
