a59b1be350ef5923bda73389e1045c33fbcea95c
Joscelyn James <josce.james7@hotmail.com>
2020-07-23 19:49:05 -0600

    feature: updated github service to get and format notifications

--- src/app/services/api/github.service.ts
+++ src/app/services/api/github.service.ts
@@ -4,6 +4,7 @@ import { environment } from '../../../environments/environment';
 
 const corsProxyApi = 'https://cors-anywhere.herokuapp.com'
 const githubApiUrl = 'https://api.github.com'
+const apiUrl = `${corsProxyApi}/${githubApiUrl}`
 
 @Injectable()
 export class GithubService {
@@ -14,17 +15,50 @@ export class GithubService {
 
   async getActivity() {
     try {
-      const { data } = await axios.get(`${corsProxyApi}/${githubApiUrl}/notifications`, {
-        auth: {
-          'username': environment.githubUsername,
-          'password': environment.githubAPIToken
-        }
-      })
-      
-      this.notifications = data
+      await this.getNotifications()
+      await this.getEvents()
     } catch (err) {
       console.error(err)
     }
+  }
+  
+  async getNotifications() {
+    const { data } = await axios.get(`${apiUrl}/notifications`, { auth: environment.auth })
+
+    const formattedNotifications = data
+      .filter(notification => notification.reason !== 'security_alert')
+      .map(async notification => {
+        return {
+          reason: notification.reason,
+          body: notification.subject.title,
+          type: notification.subject.type,
+          pullRequest: await this.getPullRequest(notification.subject.url)
+        }
+      })
+      
+    this.notifications = await Promise.all(formattedNotifications)  
+  }
+
+  async getPullRequest(url) {
+    try {
+      // the url we get already has githubs api url. we only need the cors proxy
+      const { data } = await axios.get(`${corsProxyApi}/${url}`, { auth: environment.auth })
+  
+      return {
+        opened: data.created_at,
+        link: data.html_url,
+        status: data.state,
+        assignee: data.assignee.login
+      }
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  async getEvents() {
+    const { data } = await axios.get(`${apiUrl}/users/JoscelynJames/received_events`, {
+      auth: environment.auth
+    })
   }
 }
  
