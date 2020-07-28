390f0e667df502d051c972fde44f4ca398dedf18
Joscelyn James <josce.james7@hotmail.com>
2020-07-28 09:40:33 -0600

    chore: added material chips to module

--- src/app/modules/material.module.ts
+++ src/app/modules/material.module.ts
@@ -3,17 +3,20 @@ import { NgModule } from '@angular/core'
 import { MatCardModule } from '@angular/material/card'
 import { MatListModule } from '@angular/material/list'
 import { MatIconModule } from '@angular/material/icon'
+import { MatChipsModule } from '@angular/material/chips'
 
 @NgModule({
   imports: [
     MatCardModule,
     MatListModule,
     MatIconModule,
+    MatChipsModule,
   ],
   exports: [
     MatCardModule,
     MatListModule,
     MatIconModule,
+    MatChipsModule,
   ]
 })
 export class MaterialModule { }
