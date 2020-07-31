17382e8f9e4b78c29fca45760ee5875f6d59e5bd
Joscelyn James <josce.james7@hotmail.com>
2020-07-30 20:30:05 -0600

    refactor: removed fake data and updated the data to use service get

--- src/app/components/covid-stats/covid-stats.component.ts
+++ src/app/components/covid-stats/covid-stats.component.ts
@@ -1,18 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { CovidService } from '../../services/api/covid.service';
-
+import { Component } from '@angular/core'
+import { CovidService } from '../../services/api/covid.service'
 @Component({
   selector: 'covid-stats',
   templateUrl: './covid-stats.component.html',
   styleUrls: ['./covid-stats.component.scss']
 })
-export class CovidStatsComponent implements OnInit {
-  data
 
-  constructor(private covidService: CovidService) { }
-
-  async ngOnInit() {
-    this.data = await this.covidService.getStatsForUnitedStates();
-  }
-
+export class CovidStatsComponent {
+  constructor(public covidService: CovidService) { }
 }
--- src/app/services/api/covid.service.ts
+++ src/app/services/api/covid.service.ts
@@ -1,55 +1,50 @@
 import { Injectable } from '@angular/core'
 import axios from 'axios'
-import { environment } from '../../../environments/environment';
+import { environment } from '../../../environments/environment'
 
 @Injectable()
 export class CovidService {
   public covidChartData
   private unitedStatesCovidData: Array<UnitedStatesCovidDataPoint>
 
-  constructor() {}
+  constructor() {
+    this.setStatsForUnitedStates()
+  }
 
-  async getStatsForUnitedStates() {
+  get getCovidStats(): UnitedStatesCovidDataPoint[] {
+    return this.unitedStatesCovidData
+  }
+
+  async setStatsForUnitedStates(): Promise<void> {
     try {
-      if (!environment.production) return this.fakeData()
-
       const { data } = await axios.get('https://api.covid19api.com/total/country/united-states')
-      this.unitedStatesCovidData = data
-
-      return this.formatChartData()
+      this.unitedStatesCovidData = this.formatChartData(data)
     } catch (err) {
       console.error(err)
     }
   }
 
-  formatChartData() {
-    return this.unitedStatesCovidData.map((dataPoint, i) => {
-      const casesConfirmedOnThisDay = i === 0 
+  formatChartData(data: any): UnitedStatesCovidDataPoint[]  {
+    return data.map((dataPoint: any, i) => {
+      // get the additional cases for each day
+      const casesConfirmedOnThisDay = i === 0
         ? dataPoint.Confirmed
-        : dataPoint.Confirmed - this.unitedStatesCovidData[i - 1].Confirmed
+        : dataPoint.Confirmed - data[i - 1].Confirmed
 
-      return {
+      return ({
         date: new Date(dataPoint.Date),
         name: `data-point-${i}`,
         value: dataPoint.Confirmed,
         casesConfirmedOnThisDay
-      }
+      } as unknown) as UnitedStatesCovidDataPoint
     })
   }
 
-  fakeData() {
-    let data = [];
-    let visits = 10;
-    for (let i = 1; i < 366; i++) {
-      visits += Math.round((Math.random() < 0.5 ? 1 : -1) * Math.random() * 10);
-      data.push({ date: new Date(2018, 0, i), name: "name" + i, casesConfirmedOnThisDay: visits, value: visits });
-    }
-    return data 
-  }
 }
 
-
-interface UnitedStatesCovidDataPoint {
+export interface UnitedStatesCovidDataPoint {
   Date: Date
-  Confirmed: number
+  casesConfirmedOnThisDay: number
+  value: string
+  name: string
 }
